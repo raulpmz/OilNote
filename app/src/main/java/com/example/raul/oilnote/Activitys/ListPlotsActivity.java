61de64ea53b14170e0d950758acecfbc73c79576
raulpmz <raulpm92@gmail.com>
2017-06-15 04:41:54 +0200

    Cambios

---
+++ app/src/main/java/com/example/raul/oilnote/Activitys/ListPlotsActivity.java
@@ -0,0 +1,14 @@
+package com.example.raul.oilnote.Activitys;
+
+import android.os.Bundle;
+
+import com.example.raul.oilnote.R;
+
+public class ListPlotsActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_plots);
+    }
+}
